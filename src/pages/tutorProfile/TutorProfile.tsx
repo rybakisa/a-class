4c5955e956d680d51a3d63ef8f30ac5b3ef3ab4d
tayagorbunova <gorbunovataisia@gmail.com>
2021-07-28 14:25:02 +1000

    add tutor profile

---
+++ src/pages/tutorProfile/TutorProfile.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import TutorHeader from '../../components/tutorHeader/TutorHeader';
+
+const TutorProfile = () => {
+  return (
+    <div className="tutor-profile">
+      <TutorHeader />
+    </div>
+  );
+};
+
+export default TutorProfile;
